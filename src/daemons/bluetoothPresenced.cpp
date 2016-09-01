4db51037deb7bc2c9ff478fbcb25d992c6c83ad6
Dr. Jürgen Hannappel <juergen@juergen-hannappel.de>
2016-09-01 16:42:28 +0200

    new demon that checks for bluetooth devices
    this is also a test case for bool values

---
+++ src/daemons/bluetoothPresenced.cpp
@@ -0,0 +1,52 @@
+#include "measurement.h"
+#include "slowcontrolDaemon.h"
+#include <stdlib.h>
+
+#include <Options.h>
+class presence: public SlowcontrolMeasurement<bool>,
+	public defaultReaderInterface {
+  protected:
+	std::string lCommand;
+  public:
+	presence(const std::string& aAddress):
+		SlowcontrolMeasurement(std::chrono::minutes(60),
+		                       std::chrono::seconds(60)) {
+		lCommand = "l2ping -c 1 ";
+		lCommand += aAddress;
+		std::string description(aAddress);
+		description += " presence";
+		fInitializeUid(description);
+		fConfigure();
+	};
+	virtual void fReadCurrentValue() {
+		auto result = system(lCommand.c_str());
+		switch (result) {
+			case 0:
+				fStore(true);
+				break;
+			case 1:
+				fStore(false);
+				break;
+			default:
+				break;
+		}
+	}
+};
+
+
+
+int main(int argc, const char *argv[]) {
+	OptionMap< std::string> btAdresses('a', "address", "bluetooth adresses");
+
+	OptionParser parser("slowcontrol program for reading one-wire devices vone owfs");
+	parser.fParse(argc, argv);
+
+	auto daemon = new slowcontrolDaemon("bluetoothPresenced");
+
+	for (auto& addr : btAdresses) {
+		new presence(addr.second);
+	}
+
+	daemon->fStartThreads();
+	daemon->fWaitForThreads();
+}
