0a5caec9642905dd1dfa091ce95781991e4d9057
Juergen Hannappel <juergen@juergen-hannappel.de>
2016-11-26 18:48:28 +0000

    fix typo

--- src/daemons/gpiod.cpp
+++ src/daemons/gpiod.cpp
@@ -13,7 +13,7 @@ int main(int argc, const char *argv[]) {
 	OptionMap<unsigned int> inPinNumbers('i', "inpin", "input pin name/number pairs");
 	OptionMap<unsigned int> outPinNumbers('o', "outpin", "output pin name/number pairs");
 	OptionMap<unsigned int> durationInPinNumbers('I', "durationIn", "input pin name/number pairs for duration measurements");
-	OptionMap<unsigned int> durationOutPinNumbers('O', "durationOut", "output pin name/number pairs  for durationOut measurements");
+	OptionMap<unsigned int> durationOutPinNumbers('O', "durationOut", "output pin name/number pairs  for duration measurements");
 	parser.fParse(argc, argv);
 
 	auto daemon = new slowcontrol::daemon("gpiod");
