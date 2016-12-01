6603a910e434368b728dc261a6d7e9eb8841d824
Dr. Juergen Hannappel <juergen@juergen-hannappel.de>
2016-12-01 21:08:14 +0100

    hide hidden files in videos page

--- src/website/videos.php
+++ src/website/videos.php
@@ -17,6 +17,9 @@ $videos=scandir("/data".$videodir);
 
 echo "<h1>Unclassified Videos in $videodir</h1>\n";
 foreach ($videos as $video) {
+  if ($video[0] == ".") {
+    continue;
+  }	
   $s=stat("/data".$videodir."/".$video);
   if ($s['nlink']==1) {
     echo "<a href=\"classify_video.php?video=$videodir/$video&unclassified\">$video<a><br>\n";
@@ -25,11 +28,14 @@ foreach ($videos as $video) {
 
 echo "<h1>Videos in $videodir</h1>\n";
 foreach ($videos as $video) {
+  if ($video[0] == ".") {
+    continue;
+  }	
   echo "<a href=\"classify_video.php?video=$videodir/$video\">$video<a>  \n";
 }
 
 
-echo "<h1>Sortet Videos</h1>\n";
+echo "<h1>Sorted Videos</h1>\n";
 $d = dir("/data/video/sort/");
 while(false !== ($e = $d->read())) {
   if ($e[0] != ".") {
