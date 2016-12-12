e1f7653eb026e99ebb80c70a83c18e21b979c986
Dr. Juergen Hannappel <juergen@juergen-hannappel.de>
2016-12-12 17:03:29 +0100

    ass set and skip links to classify video page

--- src/website/classify_video.php
+++ src/website/classify_video.php
@@ -24,36 +24,25 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
    }
    if (isset($_POST['olddir'])) {
       link("/data/".$video,"/data/video/sort/".$_POST['olddir']."/".basename($video));
+   } else if (isset($_POST['olddirnext'])) {
+      link("/data/".$video,"/data/video/sort/".$_POST['olddirnext']."/".basename($video));
+      $redirArgs=array("video"=>$_POST['next']);
+   } else if (isset($_POST['olddirprev'])) {
+      link("/data/".$video,"/data/video/sort/".$_POST['olddirprev']."/".basename($video));
+      $redirArgs=array("video"=>$_POST['previous']);
    }
    if (isset($_POST['deldir'])) {
       unlink("/data/video/sort/".$_POST['deldir']."/".basename($video));
    }
-}
+   if (isset($redirArgs)) {
+     if (isset($_GET['unclassified'])) {
+       $redirArgs['unclassified']="";
+     }
+     http_redirect("classify_video.php",$redirArgs,HTTP_METH_GET);
+   }
+ }
 
 echo "<video src=\"$video\" autoplay controls>$video</video>\n";
-
-$d = dir("/data/video/sort/");
-while(false !== ($e = $d->read())) {
-  if ($e[0] != ".") {
-    echo "<span>\n";
-    echo "<form action=\"classify_video.php?video=$video$uc\" method=\"post\">\n";
-    if (file_exists("/data/video/sort/".$e."/".basename($video))) {
-      $s = stat("/data/video/sort/".$e."/".basename($video));
-      if ($s['nlink']==1) {
-	echo "only exists in $e\n";
-      } else {
-	echo "Remove from <input type=\"submit\" name=\"deldir\" value=\"$e\" >, has ${s['nlink']} places to live\n";}
-    } else {
-      echo "Add to <input type=\"submit\" name=\"olddir\" value=\"$e\" >\n";
-    }
-    echo "</form>\n";
-    echo "</span>\n";
-  }
-}
-echo "<form action=\"classify_video.php?video=$video$uc\" method=\"post\">\n";
-echo "<input type=\"text\" name=\"newdir\" value=\"noname\" width=30>\n";
- echo "<input type=\"submit\" value=\"new class\" >\n";
- echo "</form>\n";
 
 for ($i=$index-1; $i>=0; $i--) {
   $file=$filedir."/".$videos[$i];
@@ -64,7 +53,8 @@ for ($i=$index-1; $i>=0; $i--) {
 	continue;
       }
     }
-    echo "<a href=\"classify_video.php?video=$videodir/${videos[$i]}$uc\">previous</a>\n";
+    $previous="$videodir/${videos[$i]}";
+    echo "<a href=\"classify_video.php?video=$previous$uc\">previous</a>\n";
     break;
   }
  }
@@ -78,10 +68,52 @@ for ($i=$index+1; $i<$nvids; $i++) {
 	continue;
       }
     }
-    echo "<a href=\"classify_video.php?video=$videodir/${videos[$i]}$uc\">next</a>\n";
+    $next="$videodir/${videos[$i]}";
+    echo "<a href=\"classify_video.php?video=$next$uc\">next</a>\n";
     break;
   }
  }
+
+echo "<table>\n";
+
+$d = dir("/data/video/sort/");
+while(false !== ($e = $d->read())) {
+  if ($e[0] != ".") {
+    echo "<tr>\n";
+    echo "<form action=\"classify_video.php?video=$video$uc\" method=\"post\">\n";
+    if (isset($next)) {
+      echo "<input type=\"hidden\" value=\"$next\" name=\"next\">\n";
+    }
+    if (isset($previous)) {
+      echo "<input type=\"hidden\" value=\"$previous\" name=\"previous\">\n";
+    }
+    if (file_exists("/data/video/sort/".$e."/".basename($video))) {
+      $s = stat("/data/video/sort/".$e."/".basename($video));
+      if ($s['nlink']==1) {
+	echo "<td colspan=3>only exists in $e</td>\n";
+      } else {
+	echo "<td colspan=2>Remove from <input type=\"submit\" name=\"deldir\" value=\"$e\" ></td><td> has ${s['nlink']} places to live</td>\n";}
+    } else {
+      if (isset($previous)) {
+	echo "<td>Add to <input type=\"submit\" name=\"olddirprev\" value=\"$e\" > and goto previous</td>\n";
+      }
+      echo "<td>Add to <input type=\"submit\" name=\"olddir\" value=\"$e\" ></td>\n";
+      if (isset($next)) {
+	echo "<td>Add to <input type=\"submit\" name=\"olddirnext\" value=\"$e\" > and goto next</td>\n";
+      }
+    }
+    echo "</form>\n";
+    echo "</tr>\n";
+  }
+}
+echo "<tr><td colspan=3>\n";
+echo "<form action=\"classify_video.php?video=$video$uc\" method=\"post\">\n";
+echo "<input type=\"text\" name=\"newdir\" value=\"noname\" width=30>\n";
+ echo "<input type=\"submit\" value=\"new class\" >\n";
+ echo "</form>\n";
+echo "</tr></td\n";
+echo "<table>\n";
+
 
 page_foot();
 ?>
