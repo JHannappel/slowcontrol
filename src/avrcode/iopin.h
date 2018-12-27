efdbae398cf2df157e1f1322f954b5253a32f7bc
Dr. Jürgen Hannappel <juergen@juergen-hannappel.de>
2018-12-27 13:05:10 +0100

    attempt to make iopins compile with new avrgcc

--- src/avrcode/iopin.h
+++ src/avrcode/iopin.h
@@ -1,6 +1,6 @@
 #ifndef __iopin_H_
 #define __iopin_H_
-
+#include <type_traits>
 
 /// template class for ioPins
 
@@ -12,34 +12,35 @@
 /// so using this class is as efficient as direct accesses, but without
 /// the risk to mess up the bits.
 /// instances of this class should be created with the define IOpin macro
-template <unsigned short portAddr,
-          unsigned short ddrAddr,
-          unsigned short inputPinsAddr,
+template <unsigned char * portAddr,
+          unsigned char * ddrAddr,
+          unsigned char * inputPinsAddr,
           unsigned char bit> class ioPin {
   public:
 	ioPin(bool isOutput) {
 		if (isOutput) {
-			_MMIO_BYTE(ddrAddr) |= _BV(bit);
+			*ddrAddr |= _BV(bit);
 		} else {
-			_MMIO_BYTE(ddrAddr) &= ~_BV(bit);
+			*ddrAddr &= ~_BV(bit);
 		}
 	};
 	/// set pin to high(true) or low(false)
 	void fSet(bool value) {
 		if (value) {
-			_MMIO_BYTE(portAddr) |= _BV(bit);
+			*portAddr |= _BV(bit);
 		} else {
-			_MMIO_BYTE(portAddr) &= ~_BV(bit);
+			*portAddr &= ~_BV(bit);
 		}
 	};
 	bool fGet() { /// read pin value, high as true, low as false
-		return (_MMIO_BYTE(inputPinsAddr) & _BV(bit) != 0);
+		return (*inputPinsAddr & _BV(bit) != 0);
 	}
 };
 
 /// macro to define an ioPin, uses preprocessor magic to produce the
 /// required register adresses from the symbolic port name.
 /// \class ioPin
-#define IOPin(Port,Bit) ioPin<reinterpret_cast<unsigned short>(&PORT##Port),reinterpret_cast<unsigned short>(&DDR##Port),reinterpret_cast<unsigned short>(&PIN##Port),Bit>
+#define portbla(x) std::remove_pointer<decltype(x)>(x)
+#define IOPin(Port,Bit) ioPin<portbla(PORT##Port),portbla(DDR##Port),portbla(PIN##Port),Bit>
 
 #endif
