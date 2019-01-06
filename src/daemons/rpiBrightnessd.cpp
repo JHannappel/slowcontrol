0303a91b696680722f66dedc8d08c223e0fa4935
Juergen Hannappel <jurgen@juergen-hannappel.de>
2019-01-06 13:51:17 +0100

    add display ctrl

--- src/daemons/rpiBrightnessd.cpp
+++ src/daemons/rpiBrightnessd.cpp
@@ -9,11 +9,10 @@
 #include <unistd.h>
 
 #include "measurement.h"
-#include "parasitic_temperature.h"
+#include "gpio.h"
 #include "slowcontrolDaemon.h"
+
 #include <Options.h>
-#include <dirent.h>
-#include <cmath>
 #include <iostream>
 #include <string.h>
 /* Puffer fuer die RTC-Daten */
@@ -108,12 +107,15 @@ class displayBrighnessWrite: public slowcontrol::measurement<short>,
 		fInitializeUid(name);
 		fConfigure();
 	};
+  void fSet(short value) {
+			std::ofstream file(lPath.c_str());
+			file << value;
+			fStore(value);
+  };
 	bool fProcessRequest(const writeValue::request* aRequest, std::string& aResponse) override {
 		auto req = dynamic_cast<const requestWithType*>(aRequest);
 		if (req != nullptr) {
-			std::ofstream file(lPath.c_str());
-			file << req->lGoalValue;
-			fStore(req->lGoalValue);
+		  fSet(req->lGoalValue);
 			aResponse = "done.";
 			return true;
 		}
@@ -129,6 +131,11 @@ class displayBrighnessWrite: public slowcontrol::measurement<short>,
 int main(int argc, const char *argv[]) {
 	options::single<const char*> deviceName('d', "device", "name of the i2c device", "/dev/i2c-1");
 	options::single<const char*> measurementName('n', "name", "name base of the measurement");
+	options::single<unsigned int> motionPin('m', "motionPin", "gpiopin for motion detector", 27);
+	options::single<std::string> startCmd('s', "startCmd","startCommand");
+	options::single<std::string> stopCmd('S', "stopCmd","startCommand");
+
+	
 	options::parser parser("slowcontrol program for reading bh1750 brightness sensors");
 	parser.fParse(argc, argv);
 
@@ -136,12 +143,49 @@ int main(int argc, const char *argv[]) {
 
 	auto daemon = new slowcontrol::daemon("rpiBrightnessd");
 
-	new bh1750brightness(deviceName, measurementName);
+	bh1750brightness bh1750(deviceName, measurementName);
 
 	displayBrighnessWrite bw("/sys/class/backlight/rpi_backlight/brightness");
 
 	displayBrighnessRead br("/sys/class/backlight/rpi_backlight/actual_brightness");
 
+	slowcontrol::watch_pack watchPack;
+	slowcontrol::watched_measurement<slowcontrol::gpio::input_value>  motionDet(watchPack,
+										  
+										    [](slowcontrol::gpio::input_value * aThat) {
+										      return aThat->fGetCurrentValue();
+										    },
+										    "DisplayPIRSense", motionPin);
+       
+	
 	daemon->fStartThreads();
+
+	auto lastPresenceTime = std::chrono::system_clock::now();
+	bool present=false;
+	while (!daemon->fGetStopRequested()) {
+	  if (watchPack.fWaitForChange()) { // motion detected
+	    lastPresenceTime = std::chrono::system_clock::now();
+	    if (!present) { // someone just appeared
+	      present = true;
+	      system(startCmd.c_str());
+	    }
+	  } else if (present) { // no motion, just timeout (or presence vanished)
+	    auto now = std::chrono::system_clock::now();
+	    if (now - lastPresenceTime > std::chrono::seconds(60)) {
+	      present = false;
+	      system(stopCmd.c_str());
+				bw.fSet(0);
+	    }
+	  }
+		if (present) {
+			short requestBrighness=12; // minimum non-zero value
+			requestBrighness += bh1750.fGetCurrentValue();
+			if (requestBrighness > 255) {
+				requestBrighness = 255;
+			}
+			bw.fSet(requestBrighness);
+		}
+	}
+	
 	daemon->fWaitForThreads();
 }
